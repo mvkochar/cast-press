356ef91d3a5dced8dd51b1f303f91ef137bc10ca
sgkochar <sgkochar@gmail.com>
2025-01-03 13:40:57 +0200

    add Team and Posts blocks to Home page, create Footer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
+import TeamMember from "./TeamMember/TeamMember";
 
 
-export { Header, Main, Footer }
+export { Header, Main, Footer, TeamMember }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/Home.css'
+import { TeamMember } from '../components'
 
 
 const Home = () => {
@@ -86,6 +87,43 @@ const Home = () => {
               </svg>
             </button>
           </div>
+        </section>
+        <section className="home-team">
+          <h2 className="home-bl-title">Team</h2>
+          <div className="home-team-box d-f">
+            <TeamMember
+              image='/images/team1.png'
+              position='Position'
+              name='Aida Cave'
+            />
+            <TeamMember
+              image='/images/team2.png'
+              position='Position'
+              name='Nick Carleton'
+            />
+          </div>
+        </section>
+        <section className='home-posts'>
+          <h2 className="home-bl-title">Latest Posts</h2>
+          <div className="home-posts-box d-f">
+            <div className="home-posts-item d-f">
+              <div><img src="/images/home-post1.png" alt="home-post1" /></div>
+              <div>
+                <h3 className="posts-item-title">How to create your own podcast cover art?</h3>
+                <p className="posts-item-date">Dec 4, 2019</p>
+                <a href="" className="posts-item-more">Read More</a>
+              </div>
+            </div>
+            <div className="home-posts-item d-f">
+              <div><img src="/images/home-post2.png" alt="home-post2" /></div>
+              <div>
+                <h3 className="posts-item-title">How to create your own podcast cover art?</h3>
+                <p className="posts-item-date">Dec 4, 2019</p>
+                <a href="" className="posts-item-more">Read More</a>
+              </div>
+            </div>
+          </div>
+          <a href="" className="home-posts-view">View Blog</a>
         </section>
       </div>
     </>
